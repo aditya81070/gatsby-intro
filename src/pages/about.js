f97fe6b34bae2dea46587eab2d76a6f4bbb117a7
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-12-27 22:49:26 +0530

    add pages and links

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'gatsby';
+export default () => {
+  return (
+    <>
+      <h1>About Me</h1>
+      <p>It is my personal website.</p>
+      <Link tp='/'>&larr; back to home</Link>
+    </>
+  );
+};
