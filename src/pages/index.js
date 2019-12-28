98cebeb4deb0fec982f431e64f4e3b02a132297d
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-12-29 00:50:28 +0530

    display posts from instagram
    In this we load data from instagram using gatsby-instagram-plugin and display it on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
+import Insta from '../components/insta';
 import Hero from '../components/hero';
 
 export default () => {
@@ -14,6 +15,7 @@ export default () => {
         {posts.map(post => (
           <PostPreview key={post.slug} post={post} />
         ))}
+        <Insta />
       </Layout>
     </>
   );
